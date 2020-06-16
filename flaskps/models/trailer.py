49b086ac2ac1bf55c46b90fa3aead1fe88490552
artemisax <62900292+artemisax@users.noreply.github.com>
2020-06-16 15:43:07 -0300

    Update trailer.py

--- flaskps/models/trailer.py
+++ flaskps/models/trailer.py
@@ -1,11 +1,53 @@
 class Trailer(object):
     db = None
     @classmethod
-    def create(cls, data):
+    def setTrailer(cls,data, titulo):
         sql = ' INSERT INTO trailer (titulo, archivo) VALUES (%s, %s)'
-        data = (data.get('titulo'), data.get('descripcion'))
+        data = (titulo, data.get('archivo'))
         cursor = cls.db.cursor()
+        cursor.execute(sql,data)
+        cls.db.commit()
+        return True
+
+
+    @classmethod
+    def create(cls,data,filename):
+        sql = ' INSERT INTO trailer (titulo,archivo) VALUES(%s,%s)'
+        data = (data.get('titulo'),filename)
+        cursor = cls.db.cursor()
+        cursor.execute(sql,data)
+        cls.db.commit()
+        return True    
+
+
+    @classmethod
+    def updateTrailer(cls,tituloNue,id):
+        sql = 'UPDATE trailer SET titulo = %s WHERE id = %s'
+        data = (tituloNue, id)
+        cursor = cls.db.cursor()    
         cursor.execute(sql, data)
         cls.db.commit()
         return True
-    
+
+    @classmethod
+    def deleteTrailer(cls,id):
+    	sql = "DELETE FROM trailer where id = %s"
+    	cursor = cls.db.cursor()
+    	cursor.execute(sql,id)
+    	cls.db.commit()
+    	return True
+
+    @classmethod
+    def getTrailers(cls):
+    	sql = 'SELECT * from trailer'
+    	cursor = cls.db.cursor()
+    	cursor.execute(sql)
+    	return cursor.fetchall()
+
+
+    @classmethod
+    def getTrailerByID(cls,id):
+        sql = 'SELECT * FROM trailer WHERE id = %s'
+        cursor = cls.db.cursor()
+        cursor.execute(sql, id)
+        return cursor.fetchone()
